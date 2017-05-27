76b558bd56afc1168561d05b2138f82e2b32c79b
dablcorta00 <dbalcorta00@berkeley.edu>
2017-05-26 18:38:25 -0700

    Create chapter 2 practice problems

---
+++ ch2/Programs/Ch2Practice.rb
@@ -0,0 +1,11 @@
+puts 'Hours in a year:' 
+puts 365*24
+puts ' '
+puts 'Minutes in a decade:'
+puts 10*365*24*60
+puts ' '
+puts 'My age in seconds:'
+puts 17*365*24*60*60
+puts ' '
+puts 'Author\'s age:'
+puts 1160000000/60/60/24/365
