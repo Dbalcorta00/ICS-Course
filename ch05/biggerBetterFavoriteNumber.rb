fefa723bb19c7fec295d734967e4a85911cc6e1b
dablcorta00 <dbalcorta00@berkeley.edu>
2017-05-31 20:33:35 -0700

    Work on chapter 5 assignments

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,5 @@
+puts 'Hey there human! What\'s your favorite number?'
+fnum = gets.chomp
+puts 'Oh really? Your favorite number is ' + fnum + '?'
+puts 'That\'s funny because my favorite number is ' + (fnum.to_i + 1).to_s + ','
+puts 'which as you know is a BIGGER and BETTER favorite number!'
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,15 @@
+puts 'Hey there human! I\'m going to need to know your name alright?'
+puts ''
+puts 'Alright first things first, what\'s your first name?'
+puts ''
+fname = gets.chomp
+puts ''
+puts 'Sweet! I\'m also gonna need your middle name.'
+puts ''
+mname = gets.chomp
+puts ''
+puts 'Okay for the cherry on top, I\'m going to need your last name.'
+puts ''
+lname = gets.chomp
+puts ''
+puts 'So your name is ' + fname + ' ' + mname + ' ' + lname + '? Awesome, nice to meet you!'
